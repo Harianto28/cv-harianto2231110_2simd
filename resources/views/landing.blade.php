4a8727af4e94cc956d7f705e40f5d20ef3878611
Harianto28 <haryantoqz.gmail.com>
2023-03-17 23:47:31 +0700

    first

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Harianto | CV</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body class="" id="home`">
+    <!-- Navbar -->
+    <nav
+      class="navbar navbar-expand-lg navbar-dark fixed-top"
+      style="background-color: #1d3557"
+    >
+      <div class="container">
+        <a class="navbar-brand" href="#">Harianto | CV</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#home"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#experience">Experience</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#education">Education</a>
+            </li>
+            <!-- <li class="nav-item">
+              <a class="nav-link" href="#">Disabled</a>
+            </li> -->
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- End of Navbar -->
+    <!-- Home -->
+    <section class="jumbotron text-center" id="home">
+      <img
+        src="images/profile.jpg"
+        alt=""
+        width="200px"
+        class="rounded-circle img-thumbnail"
+      />
+      <h1 class="display-4">Harianto</h1>
+      <p class="lead">Student</p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#ffff"
+          fill-opacity="1"
+          d="M0,224L48,218.7C96,213,192,203,288,213.3C384,224,480,256,576,234.7C672,213,768,139,864,106.7C960,75,1056,85,1152,122.7C1248,160,1344,224,1392,256L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+    <!-- End of Home -->
+    <!-- About Me -->
+    <section id="about">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>About Me</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+          <div class="col-md-4 mb-2 mx-4">
+            Nama saya Harianto. sekarang saya sedang berkuliah di Universitas
+            Internasional Batam, dengan Prodi Sistem Informasi.
+          </div>
+          <div class="col-md-4 mx-4">
+            Saya mengambil Prodi Sistem Informasi, untuk mempelajari dan
+            memperdalam ilmu komputer.
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#ade8f4"
+          fill-opacity="1"
+          d="M0,256L60,256C120,256,240,256,360,245.3C480,235,600,213,720,197.3C840,181,960,171,1080,186.7C1200,203,1320,245,1380,266.7L1440,288L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+    <!-- End of About Me -->
+    <!-- Experience -->
+    <section id="experience">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>Experience</h2>
+          </div>
+        </div>
+        <div class="row justify-content-evenly">
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="images/gaming.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Coding 1 day.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="images/gaming.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Coding 2 day.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="images/gaming.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Coding 3 day.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="images/gaming.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Coding 4 day.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="images/gaming.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Coding 5 day.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#ffff"
+          fill-opacity="1"
+          d="M0,96L40,85.3C80,75,160,53,240,58.7C320,64,400,96,480,122.7C560,149,640,171,720,170.7C800,171,880,149,960,160C1040,171,1120,213,1200,208C1280,203,1360,149,1400,122.7L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+    <!-- End of Experience -->
+    <section id="education">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>Education</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="images/school.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">SMK Plus Kemilau Bangsa Tiban</p>
+                <p>2019 - 2022</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <img src="images/university.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <p class="card-text">Universitas Internasional Batam</p>
+                <p>2022 - Sekarang</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#0099ff"
+          fill-opacity="1"
+          d="M0,224L48,202.7C96,181,192,139,288,117.3C384,96,480,96,576,122.7C672,149,768,203,864,218.7C960,235,1056,213,1152,208C1248,203,1344,213,1392,218.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
+    <footer
+      class="text-white text-center p-3"
+      style="background-color: #0099ff"
+    >
+      <p>
+        Created By
+        <a href="https://www.instagram.com/_hryy_/" class="text-white fw-bold"
+          >Harianto</a
+        >
+      </p>
+    </footer>
+  </body>
+
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"
+  ></script>
+</html>
